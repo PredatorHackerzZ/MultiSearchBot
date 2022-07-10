41d74b8653777afc70307ffc5cb71d3c5e3ac4f7
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-07-10 21:53:00 +0530

    Update callback.py

--- MultiSearchBot/callback.py
+++ MultiSearchBot/callback.py
@@ -11,25 +11,25 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQ
 
 @Bot.on_callback_query()
 async def button(_, message: Message):
-    if update.data == "home":
+    if message.data == "home":
         await message.edit_text(
             text=Script.START_TEXT.format(update.from_user.mention),
             reply_markup=Script.START_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "help":
+    elif message.data == "help":
         await message.edit_text(
             text=Script.HELP_TEXT,
             reply_markup=Script.HELP_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "about":
+    elif message.data == "about":
         await message.edit_text(
             text=Script.ABOUT_TEXT,
             reply_markup=Script.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
-    elif update.data == "inline_buttons":
+    elif message.data == "inline_buttons":
         await message.edit_text(
             text=Script.INLINE_TEXT,
             reply_markup=Script.SEARCH_BUTTONS,
